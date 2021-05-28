e4cc09004fdd86c1f50391ff2196d934bc5f8e0a
RandomMallard <81260075+RandomMallard@users.noreply.github.com>
2021-05-28 15:11:26 +0100

    First commit

---
+++ WordGame/WordGame/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace WordGame
+{
+    class Process
+    {
+        static void Main(string[] args)
+        {
+            int Game = 0;
+
+
+            Console.WriteLine("Welcome to the word game!");
+            Console.WriteLine("This is essentially hangman but I'm not gonna call it that because there is probably a copyright or trademark on it somewhere.");
+            Console.WriteLine("Please make a choice to proceed:");
+            Console.WriteLine("[0] Quit the game            [1]  Play the game          [2] Another option");
+            Console.WriteLine("Please choose here:");
+            Game = Convert.ToInt32(Console.ReadLine());
+
+
+            if (Game == 0)
+            {
+                Console.WriteLine("Goodbye");
+            }
+            else if (Game == 2)
+            {
+                Console.WriteLine("This would have been a rickroll but it didn't work");
+            }
+        }
+
+    }
+}
